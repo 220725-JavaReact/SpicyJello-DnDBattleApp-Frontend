bea9250161cc0c39c9770faa83a87acc5522d416
Warren Lee <wplee327@users.noreply.github.com>
2022-09-16 23:59:23 -0400

    Create react project; Create models; Create test page for REST API connection

---
+++ dndbattleappfe/src/models/Game.tsx
@@ -0,0 +1,10 @@
+import { IMonster } from "./Monster";
+import { IUser } from "./User";
+
+export interface IGame {
+    id?: number,
+    user: IUser,
+    score: number,
+    gold: number,
+    monsters: IMonster[]
+}
---
+++ dndbattleappfe/src/models/Monster.tsx
@@ -0,0 +1,8 @@
+import { IGame } from "./Game";
+
+export interface IMonster {
+    id?: number,
+    game: IGame,
+    source: string,
+    modifier: string
+}
