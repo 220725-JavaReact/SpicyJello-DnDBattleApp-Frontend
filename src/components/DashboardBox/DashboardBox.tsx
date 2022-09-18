0e3db67974b57d3f243f4aa14420b10ac6a5b508
Warren Lee <wplee327@users.noreply.github.com>
2022-09-18 17:52:34 -0400

    Add navbar

--- src/components/DashboardBox/DashboardBox.tsx
+++ src/components/DashboardBox/DashboardBox.tsx
@@ -1,4 +1,5 @@
 import { Navigate } from "react-router";
+import NavBar from "../../shared/NavBar/NavBar";
 import { useAppDispatch, useAppSelector } from "../../shared/Redux/hook";
 import { selectUser } from "../LoginBox/UserSlice";
 
@@ -7,6 +8,7 @@ function DashboardBox() {
     const user = useAppSelector(selectUser);
     if (user.username === "") return <Navigate replace to="/" />;
     else return <div>
+        <NavBar />
         <p>Welcome to your dashboard, {user.username}.</p>
     </div>;
 }
---
+++ src/shared/NavBar/NavBar.tsx
@@ -0,0 +1,31 @@
+import { Link, Navigate } from "react-router-dom";
+import { resetUser, selectUser } from "../../components/LoginBox/UserSlice";
+import { useAppDispatch, useAppSelector } from "../Redux/hook";
+
+function NavBar() {
+    const dispatch = useAppDispatch();
+    const user = useAppSelector(selectUser);
+    function logout() {
+        dispatch(resetUser());
+    }
+    if (user.username === "") return <Navigate replace to="/" />;
+    else return <nav className="navbar">
+        <div className="container-fluid">
+            <div className="navbar-header">
+                <Link to="/dashboard" className="navbar-brand">DnD Battle App</Link>
+            </div>
+            <ul className="nav navbar-nav">
+                <li className="nav-item">
+                    <Link to="/upgrades" className="nav-link disabled">Upgrades</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/settings" className="nav-link disabled">Settings</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/" onClick={logout} className="nav-link">Logout</Link>
+                </li>
+            </ul>
+        </div>
+    </nav>;
+}
+export default NavBar;
